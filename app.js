35cb83ed2a0885b74a1096aa6f96102aaebfe104
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-04-21 01:25:50 +0100

    first commit

--- app.js
+++ app.js
@@ -12,11 +12,23 @@ if (navigator.geolocation) {
       let result = await data;
       result = await result.json();
       outputData(result);
-      console.log(result.main);
+      //   console.log(result.main);
     }
     getWeather();
   });
 }
+let loader = document.querySelector(".load");
 let resultDiv = document.querySelector(".result");
 let cityName = document.querySelector("cityname");
 let getWeatherBtn = document.querySelector(".getbtn");
+
+async function getWeather() {
+  loader.style.display = "block";
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${cityName.value}&appid=${apiKey}`;
+  let data = fetch(api);
+  let result = await (await data).json();
+  loader.style.display = "none";
+  resultDiv.style.display = "block";
+  outputData(result);
+}
+getWeatherBtn.addEventListener("click", getWeather);
